545b5f118bc03f973370bc78fe954dd519ea0e6f
Raj Parmar <58849801+rajparmarr2308@users.noreply.github.com>
2020-08-09 15:37:38 +0530

    Update settings.py

--- mac/settings.py
+++ mac/settings.py
@@ -19,10 +19,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '9h_#wy*6)%#ug3-uv@7xlryan5a36rqe^j5a$-i0@fo9szu=%n'
 
-# SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
 ALLOWED_HOSTS = []
